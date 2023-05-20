72c60f094b474f985336cac06b8d647421961c51
TerenceGrover <terencegrover@gmail.com>
2023-05-21 00:35:50 +0200

    comments

--- script.py
+++ script.py
@@ -12,6 +12,15 @@ check_parser = subparsers.add_parser('check', help='check password strength')
 check_parser.add_argument('password', type=str, help='password to check')
 
 def check_password(password):
+    # Here are the ideas.
+    # 1. Check if password is longer than 8 characters
+    # 2. Check if password contains at least one digit
+    # 3. Check if password contains at least one uppercase letter
+    # 4. Check if password contains at least one lowercase letter
+    # 5. Check if password contains at least one special character
+    # Once those are done, return a score between 0 and 10
+    # Offer alternative passwords if the score is low which can be selected with digits in the command line. This will copy the password to the clipboard.
+
     return password
 
 
